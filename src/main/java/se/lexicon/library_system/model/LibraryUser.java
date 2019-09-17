c14127b2a4724dbc0ecc072fa52ecbbc5cdcb6ae
Erik Svensson <eosvensson@gmail.com>
2019-09-17 10:47:37 +0200

    Updated with LibraryUser Entity

---
+++ src/main/java/se/lexicon/library_system/model/LibraryUser.java
@@ -0,0 +1,116 @@
+package se.lexicon.library_system.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class LibraryUser {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private int id;
+	private String firstName;
+	private String lastName;
+	private LocalDate birthDate;
+	private String email;
+	
+	public LibraryUser(int id, String firstName, String lastName, LocalDate birthDate, String email) {
+		this(firstName, lastName, birthDate, email);
+		this.id = id;		
+	}
+
+	public LibraryUser(String firstName, String lastName, LocalDate birthDate, String email) {
+
+		setFirstName(firstName);
+		setLastName(lastName);
+		setBirthDate(birthDate);
+		setEmail(email);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LibraryUser other = (LibraryUser) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("LibraryUser [id=");
+		builder.append(id);
+		builder.append(", firstName=");
+		builder.append(firstName);
+		builder.append(", lastName=");
+		builder.append(lastName);
+		builder.append(", birthDate=");
+		builder.append(birthDate);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append("]");
+		return builder.toString();
+	}
+}
